d3894242d4d376ea4a82d860b457e24f5bf05a67
TR742 <Tyrich1442@gmail.com>
2022-04-22 00:01:40 -0700

    Updated product-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,8 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories
+// be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
